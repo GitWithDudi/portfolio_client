11b87bdbf969fa0ebb41b9181ec8aaae2a597d48
GitWithDudi <dudi.gitar@gmail.com>
2025-10-19 23:14:07 +0300

    Merge main into trying

--- src/Components/Layout_area/NavBar/NavBar.tsx
+++ src/Components/Layout_area/NavBar/NavBar.tsx
@@ -4,11 +4,22 @@ import styles from "./NavBar.module.css";
 export function NavBar(): React.JSX.Element {
   return (
     <nav className={styles.navbar}>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/projects">Projects</Link>
-      <Link to="/recommendations">Recommendations</Link>
+      {/* Checkbox מוסתר לשליטה על ההמבורגר */}
+      <input type="checkbox" id="nav-toggle" className={styles["nav-toggle"]} />
+
+      {/* כפתור המבורגר */}
+      <label htmlFor="nav-toggle" className={styles.hamburger}>
+        ☰
+      </label>
+
+      {/* הקישורים */}
+      <div className={styles.links}>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/recommendations">Recommendations</Link>
+      </div>
     </nav>
   );
 }
